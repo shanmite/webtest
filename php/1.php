d8ef00b90ed238db67273bf079105d0de88f65d6
shanmite <1971513084@qq.com>
2020-05-25 18:43:18 +0800

    新增1.php

---
+++ php/1.php
@@ -0,0 +1,17 @@
+<?php
+
+// const P = 'P';
+// define('p', 'p');
+// define('-_-', '-_-');
+// echo 'PHP';
+// echo '<hr>';
+// echo <<<EOF
+// <p>da</p>
+// <br>
+// EOF;
+// echo 'eewe';
+// echo '';
+// echo __FILE__;
+// echo P;
+// echo constant('-_-');
+?>
