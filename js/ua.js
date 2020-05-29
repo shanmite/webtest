5b755454ae9ed33f33593d876c4c4262f7dfbb40
shanmite <1971513084@qq.com>
2020-05-30 00:44:28 +0800

    [JavaScript]新增UA识别

---
+++ js/ua.js
@@ -0,0 +1,31 @@
+function IsPC() {
+    var userAgentInfo = navigator.userAgent;
+    var Agents = ["Android", "iPhone",
+                "SymbianOS", "Windows Phone",
+                "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+var u = navigator.userAgent;
+    if (u.indexOf('Android') > -1 || u.indexOf('Linux') > -1) {
+        document.getElementById("css").href= "";
+        //Android css
+    } else if (u.indexOf('iPhone') > -1) {
+        //苹果手机
+    } else if (u.indexOf('Windows Phone') > -1) {
+        //winphone手机
+    }
+    function isWeiXin(){
+        var ua = navigator.userAgent.toLowerCase();
+        if(ua.indexOf('micromessenger') != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
