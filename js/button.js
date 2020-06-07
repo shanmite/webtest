a9ceadae5d6c3c1c7d35de5140b3d066685d40ef
shanmite <1971513084@qq.com>
2020-06-07 17:04:00 +0800

    文件结构调整

--- js/button.js
+++ js/button.js
@@ -1,19 +1,19 @@
 function f() {
     document.getElementById("ok").innerHTML="スクショ";
-    document.getElementById("pic").src="html/images/aquashion.jpg"
+    document.getElementById("pic").src="images/aquashion.jpg"
 }
 function f2() {
     document.getElementById("ok").innerHTML="图片";
-    document.getElementById("pic").src="html/images/盐水.jpg"
+    document.getElementById("pic").src="images/盐水.jpg"
 }
 $(document).ready(function(){
     $("#korone").click(
         function () {
-            if (document.getElementById("korone").attributes["src"].value=="html/images/korone0.png") {
-                document.getElementById("korone").src="html/images/korone1.png"
+            if (document.getElementById("korone").attributes["src"].value=="images/korone0.png") {
+                document.getElementById("korone").src="images/korone1.png"
                 document.getElementById("korone").title="あ~!↗あ~!↗あ~!↗"
             } else {
-                document.getElementById("korone").src="html/images/korone0.png"
+                document.getElementById("korone").src="images/korone0.png"
                 document.getElementById("korone").title="ほら見てほらよ"
             }
         }
